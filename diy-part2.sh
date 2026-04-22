46db3fffd16012cbd3910ab07ba065583959bbcc
mochen100 <69398849+mochen100@users.noreply.github.com>
2026-04-22 17:28:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,4 +8,19 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# 动态追加所需插件到 .config
+cat >> .config <<EOF
+# WireGuard 及相关依赖 (luci-app 和 接口协议)
+CONFIG_PACKAGE_luci-app-wireguard=y
+CONFIG_PACKAGE_luci-proto-wireguard=y
+
+# HomeProxy
+CONFIG_PACKAGE_luci-app-homeproxy=y
+
+# EasyTier
+CONFIG_PACKAGE_luci-app-easytier=y
+
+# Tailscale
+CONFIG_PACKAGE_luci-app-tailscale=y
+EOF
+
